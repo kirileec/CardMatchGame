2f31115c055e6ffd0c573b42d6a3e331a67e7a0a
Kirile <kirile@Macbook-Pro.lan>
2015-06-20 10:53:56 +0800

    Initial Commit

---
+++ CardMatchGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardMatchGame
+//
+//  Created by Kirile on 15/6/20.
+//  Copyright (c) 2015年 Kirile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
